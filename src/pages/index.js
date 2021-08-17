e5a3bf140d6ab43e883aa2125c9ee9f71e2b7067
Sergey Shaporov <sshaporov86@gmail.com>
2021-08-17 16:22:43 +0300

    show links on the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,49 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const { nodes } = data.allMarkdownRemark;
+
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <StaticImage
+        src="../images/gatsby-astronaut.png"
+        width={300}
+        quality={95}
+        formats={["AUTO", "WEBP", "AVIF"]}
+        alt="A Gatsby astronaut"
+        style={{ marginBottom: `1.45rem` }}
+      />
+      <div>
+        {nodes.map((post) => {
+          const { category, title, url } = post.frontmatter
+          return <Link key={post.id} to={`/${category}/${url}`}>{title}</Link>
+        })}
+      </div>
+    </Layout>
+)}
 
 export default IndexPage
+
+export const query = graphql`
+  query mainPage {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          url
+          title
+          category
+        }
+        id
+      }
+    }
+  }
+`
